c2b463db4acece5de985a7a93d8f73c55bbff2ae
Si Sheng <sisheng0x7c@outlook.com>
2024-11-02 20:04:02 +0800

    -

--- frontend/scripts/templates.js
+++ frontend/scripts/templates.js
@@ -1,6 +1,6 @@
 (function () {
     const sign_in_button = document.querySelector(".sign-in-up-container");
     sign_in_button.onclick = function (_e) {
-        window.location.assign("/sign_in_page.php");
+        window.location.assign("./sign_in_page.php");
     }
 })();
