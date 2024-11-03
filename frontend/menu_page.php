8084b5c613c2decb4232f97afe348d582e283fe1
brofegroy <threeusb@gmail.com>
2024-11-03 22:14:58 +0800

    pulling

--- frontend/menu_page.php
+++ frontend/menu_page.php
@@ -10,18 +10,32 @@ aquire_username_or_default(DEFAULT_USERNAME);
 
 function generate_menu_page():string{
     $display_category=$_GET['display_category']??'buffet';
+    $current_page=$_GET['current_page']??'1';
     ob_start(); //start buffer to collect generated html lines
     ?>
     
     <div class="main-content-container">
         <!-- for buffet -->
-        <button class='display-category-toggle-container' <?=$display_category=='buffet'?'disabled':'';?>></button>
+        <button 
+            class='display-category-toggle-container' <?=$display_category=='buffet'?'disabled':'';?>
+            onclick="redirectback(display_category='buffet')">
+            <label>Buffet</label>
+        </button>
 
     
         <!-- for bento -->
-        <button class='display-category-toggle-container' <?=$display_category=='bento'?'disabled':'';?>></button>
+        <button 
+            class='display-category-toggle-container' <?=$display_category=='bento'?'disabled':'';?>
+            onclick="redirectback(display_category='bento')">
+            <label>Bento</label>
+        </button>
 
     </div>
+    <script>
+        function redirectback(display_category = <?=$display_category?>){
+            window.location.href = "menu_page.php?display_category=" + display_category;
+        }
+    </script>
     
     <?php
     return ob_get_clean(); //Stop the buffer and pass the collected html to page template
